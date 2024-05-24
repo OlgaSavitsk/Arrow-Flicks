106f6d4cd78e6dafc5c6edf39d06401a28ccd9e9
Olga <olbonya.89@mail.ru>
2024-05-24 21:21:25 +0300

    fix: app-layaout file for deploy

--- src/components/app-layout/index.tsx
+++ src/components/app-layout/index.tsx
@@ -2,6 +2,8 @@ import { useRouter } from 'next/router';
 import NotFoundPage from '@pages/404';
 import { RoutePath } from '@constants/index';
 
+import { Suspense } from 'react';
+import LoaderComponent from '@components/loader';
 import MainLayout from './main-layout';
 
 const pageLayout = {
@@ -21,9 +23,11 @@ const LayoutComponent: React.FC<LayoutProps> = ({ children }) => {
   const Layout = pageLayout[route as RoutePath];
 
   return (
-    <Layout>
-      {children}
-    </Layout>
+    <Suspense fallback={<LoaderComponent />}>
+      <Layout>
+        {children}
+      </Layout>
+    </Suspense>
   );
 };
 
